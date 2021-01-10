1834229fb8c3136b5b36bff8f6cb3a4f5bc70f14
grahamkeith <68223150+grahamkeith@users.noreply.github.com>
2021-01-10 13:16:49 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 var informationStrings = {
+  //This dictionary holds the relevant string information to be displayed for each slider/button of the synthesiser
+
   ampSlideA:
     "This slider changes the time that it takes a note to reach maximum volume.",
   ampSlideD:
@@ -25,10 +27,13 @@ var informationStrings = {
   sawtoothButton: "Sets the basic waveform to a bright sawtooth wave."
 };
 
+//setting the synth panel to be invisible when the website is loaded
 document.getElementById("synth").style.display = "none";
-document.getElementById("helpText").style.color = "grey";
+
+//setting the default paramater for the synth waveform
 document.getElementById("squareButton").checked = true;
 
+//storing the default infoText, and settonhg the evet listeners for all of the parameters in the dictionary
 let infoText = "Hover over a slider or button for information.";
 
 let buttonTextColor = document.getElementById("whyImApplyingButton").style
@@ -39,6 +44,7 @@ for (const item in informationStrings) {
 }
 
 function applyInfoListeners(sliderName) {
+  //this function sets the event listeners for the mouse eneter and mousleave events for the given sliderName (also works for buttons)
   if (document.getElementById(sliderName) != null) {
     document.getElementById(sliderName).addEventListener("mouseenter", () => {
       document.getElementById("helpText").innerText =
@@ -54,19 +60,34 @@ function applyInfoListeners(sliderName) {
   }
 }
 
+//making event listeners for the navigation panel
+
 document.getElementById("whyImApplyingButton").addEventListener("click", () => {
   openApply();
 });
 
 document.getElementById("openSynthButton").addEventListener("click", () => {
   openSynth();
-  console.log("opensynthclicked");
+});
+
+document.getElementById("synthLink").addEventListener("click", () => {
+  openSynth();
+});
+
+document.getElementById("synthLink").addEventListener("mouseenter", () => {
+  document.getElementById("synthLink").style.color = "orange";
+});
+
+document.getElementById("synthLink").addEventListener("mouseleave", () => {
+  document.getElementById("synthLink").style.color = "blue";
 });
 
 function openApply() {
+  //function to open the "Why I'm Applying" section and make sure that "Play a Syntheiser" is closed
   var x = document.getElementById("whyImApplying");
   var y = document.getElementById("synth");
 
+  //once pressed, the button stays orange
   document.getElementById("whyImApplyingButton").style.color = "orange";
   document.getElementById("openSynthButton").style.color = buttonTextColor;
 
@@ -75,9 +96,11 @@ function openApply() {
 }
 
 function openSynth() {
+  //function to open the "Play a Syntheiser" section and make sure that "Why I'm Applying" is closed
   var x = document.getElementById("synth");
   var y = document.getElementById("whyImApplying");
 
+  //once pressed, the button stays orange
   document.getElementById("whyImApplyingButton").style.color = buttonTextColor;
   document.getElementById("openSynthButton").style.color = "orange";
 
